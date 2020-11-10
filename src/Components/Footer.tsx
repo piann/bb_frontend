b2eb8d1fe6bec8d457e92e4185ff36de356899c1
piann <shadow_hat@naver.com>
2020-11-10 19:23:43 +0900

    add links in Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import { Divider } from 'rsuite';
 
 const FooterWrapper = styled.footer`
     position: absolute;
@@ -12,10 +14,15 @@ const FooterWrapper = styled.footer`
     text-align: left;
     padding-left:calc(5vw + 25px);
     word-break: keep-all;
-    line-height:1.5rem;
+    line-height:2rem;
     white-space: pre-line;
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
     @media only screen and (max-width: ${props=>props.theme.mobileWidth}) {
       font-size:11px;
+      flex-direction:column;
+      justify-content:flex-start;
     }
 `;
 
@@ -35,25 +42,107 @@ const RelativeFooterWrapper = styled.footer`
     padding-left: calc(5vw + 25px);	
     text-align: left;
     word-break: keep-all;
-    line-height:1.5rem;
+    line-height: 2rem;
     white-space: pre-line;
+    display:flex;
+    flex-direction:row;
+    justify-content:space-between;
     @media only screen and (max-width: ${props=>props.theme.mobileWidth}) {
+      flex-direction:column;
+      justify-content:flex-start;
       bottom: calc(-680px - 7vh);
       padding-left: calc(2vw + 20px);	
       font-size:11px;
     }
 `;
 
+const CopyText = styled.div`
+  margin-right:100px;
+  @media only screen and (max-width: ${props=>props.theme.mobileWidth}) {
+    margin-right:0px;
+    }
+`;
+
+const Links = styled.div`
+  display:flex;
+  flex-direction:row;
+  padding-right:25px;
+`;
+
+
+const SLink = styled(Link)`
+  text-decoration:none;
+  ${props=>props.theme.snowyGrayColor}
+  &:active{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:hover{
+    color:${props=>props.theme.textColor}
+  }
+  &:visited{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:link{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:active{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  a {
+    text-decoration: none;
+  }
+  
+`;
+
+
+const OLink = styled.a`
+  text-decoration:none;
+  ${props=>props.theme.snowyGrayColor}
+  &:active{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:hover{
+    color:${props=>props.theme.textColor}
+  }
+  &:visited{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:link{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  &:active{
+    color:${props=>props.theme.snowyGrayColor}
+  }
+  a {
+    text-decoration: none;
+  }
+  
+`;
+
+const Divide = styled.div`
+  margin-left:20px;
+  margin-right:20px;
+`;
+
+
 type FooterProps = {};
 
 const FooterImage = styled.div`
-
 `;
 
 export const Footer: React.FC<FooterProps> = () => (
   <div>
     <FooterWrapper>
+    <CopyText>
         {"Copyright © pastel planet. All rights reserved."}
+    </CopyText>
+    <Links>
+      <SLink to="/privacy">{"Privacy"}</SLink>
+      <Divide>{"|"}</Divide>
+      <SLink to="/terms_of_service">{"Terms"}</SLink>
+      <Divide>{"|"}</Divide>
+      <OLink href={"https://docs.google.com/spreadsheets/d/1X7M9O6HcQ0cWdY957JxEsQuxNbNSzRGM3seZLfl_uBo/edit?usp=sharing"} target="_blank" rel={"noopener"}>{"OSL"}</OLink>
+    </Links>
     </FooterWrapper>
   </div>
 );
@@ -61,7 +150,16 @@ export const Footer: React.FC<FooterProps> = () => (
 export const RelFooter: React.FC<FooterProps> = () => (
   <div>
     <RelativeFooterWrapper>
+    <CopyText>
         {"Copyright © pastel planet. All rights reserved."}
+    </CopyText>
+    <Links>
+      <SLink to="/privacy">{"Privacy"}</SLink>
+      <Divide>{"|"}</Divide>
+      <SLink to="/terms_of_service">{"Terms"}</SLink>
+      <Divide>{"|"}</Divide>
+      <OLink href={"https://docs.google.com/spreadsheets/d/1X7M9O6HcQ0cWdY957JxEsQuxNbNSzRGM3seZLfl_uBo/edit?usp=sharing"} target="_blank" rel={"noopener"}>{"OSL"}</OLink>
+    </Links>
     </RelativeFooterWrapper>
   </div>
 );
